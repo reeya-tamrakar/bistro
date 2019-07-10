17a167afb87fe6fe915aa3bb01846938e8d5a00e
Aashish Tamrakar <praash47@gmail.com>
2019-07-10 20:35:25 +0545

    header and index update

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php echo "Hello world including reeya!"; ?>
+<?php include('header.php') ?>
+<h1>Welcome to Bistro!</h1>
+<section>
+	<h2>Slider</h2>
+	<p>We will place our slider over here.</p>
+</section>
+</body>
+</html>
