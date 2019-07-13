cb916c1f7467e6bbb1262f5495f8628bc72fe472
Aashish Tamrakar <praash47@gmail.com>
2019-07-13 10:36:32 +0545

    added logo

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php include('header.php') ?>
-
+<div>
+</div>
 <section><!-- Welcome Section -->
 	<div class="content"><!-- Content Container -->
+	<img src="images/logo/logo.png" width="150">
 	<h1>Welcome to Bistro!</h1>
 	<p>Enjoy good food. Instantly!<br><br>Here at bistro, we provide best food at your doorsteps!</p>
 	</div>
