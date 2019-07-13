c5a9557134a607b3c74c6fb7936517eea793b57c
Aashish Tamrakar <praash47@gmail.com>
2019-07-13 15:23:05 +0545

    removed unnecessar and added menu, classified everything

--- header.php
+++ header.php
@@ -4,6 +4,134 @@
 	<title>Bistro - good food instantly</title>
 
 <!-- Links -->
-	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="stylesheet" type="text/css" href="main.css"><!-- Main CSS -->
+	<link rel="stylesheet" type="text/css" href="/bistro/css/bootstrap.min.css"><!-- Bootstrap CSS -->
+
+
+
+<!-- Scripts -->
+	<script src="/bistro/icons/friconix.js"></script><!-- Icons from friconix.com -->
+	<script src="/bistro/js/jquery.js"></script><!-- jQuery JS -->
+	<script src="/bistro/js/bootstrap.min.js"></script><!-- Bootstrap JS -->
+	<script src="/bistro/js/mega-menu.js"></script><!-- Bootstrap JS -->
+
 </head>
 <body>
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+  <a href="#"><img src="/bistro/images/logo/logo.png" width="150"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="nav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active mr-4">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="dropdown menu-large nav-item"> <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">Menu </a>
+                    <ul class="dropdown-menu megamenu">
+                    	<h1 class="text-center">Mega Menu</h1>
+                        <div class="row">
+                            <li class="col-md-3 dropdown-item">
+                                <ul>
+                                    <li class="dropdown-header">Cuisine Cat 1</li>
+                                    <li><a href="#">Cold Drink</a>
+                                    </li>
+                                    <li class="disabled"><a href="#">Beer</a>
+                                    </li>
+                                    <li><a href="#">Juice</a>
+                                    </li>
+                                    <li class="divider"></li>
+                                    <li class="dropdown-header">Cuisine Cat 2</li>
+                                    <li><a href="#">Meat</a>
+                                    </li>
+                                    <li><a href="#">Sausage</a>
+                                    </li>
+                                    <li><a href="#">Choupsey</a>
+                                    </li>
+                                    <li><a href="#">Pan Parag</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="col-md-3 dropdown-item">
+                                <ul>
+                                    <li class="dropdown-header">Cuisine Cat 3</li>
+                                    <li><a href="#">Chocolates</a>
+                                    </li>
+                                    <li><a href="#">Chow-chow</a>
+                                    </li>
+                                    <li><a href="#">Biscuit</a>
+                                    </li>
+                                    <li><a href="#">Dalmoth</a>
+                                    </li>
+                                    <li><a href="#">Chatpat</a>
+                                    </li>
+                                    <li class="divider"></li>
+                                    <li class="dropdown-header">Special Cuisine</li>
+                                    <li><a href="#">Panipuri</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="col-md-3 dropdown-item">
+                                <ul>
+                                    <li class="dropdown-header">Cuisine Cat 4</li>
+                                    <li><a href="#">Tharlak</a>
+                                    </li>
+                                    <li><a href="#">Thenduk</a>
+                                    </li>
+                                    <li><a href="#">Thukpa</a>
+                                    </li>
+                                    <li class="divider"></li>
+                                    <li class="dropdown-header">Cuisine Cat 5</li>
+                                    <li><a href="#">Momo</a>
+                                    </li>
+                                    <li><a href="#">Chowmein</a>
+                                    </li>
+                                    <li><a href="#">Burger</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="col-md-3 dropdown-item">
+                                <ul>
+                                    <li class="dropdown-header"></li>
+                                    <li><a href="#">Learn Bootstrap</a>
+                                    </li>
+                                    <li><a href="#">Buttons</a>
+                                    </li>
+                                    <li><a href="#">Text</a>
+                                    </li>
+                                    <li><a href="#">Non-nav links</a>
+                                    </li>
+                                    <li><a href="#">Component alignment</a>
+                                    </li>
+                                    <li><a href="#">Fixed to top</a>
+                                    </li>
+                                    <li><a href="#">Fixed to bottom</a>
+                                    </li>
+                                    <li><a href="#">Static top</a>
+                                    </li>
+                                    <li><a href="#">Inverted navbar</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </div>
+                    </ul>
+                </li>
+      <li class="nav-item mr-4">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item mr-4">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item mr-4 hidden-md">
+      	<div class="input-group mb-2">
+		  <input type="text" class="form-control" placeholder="What's on tastebud?">
+		  <div class="input-group-append"><button type="button" class="btn btn-success"><i class="fi-xnsuxl-search"></i></button></span>
+		  </div>
+		</div>
+      </li>
+      
+
+       </a>
+    </ul>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php include('header.php') ?>
-<div>
+<div class="container">
+	<div class="jumbotron">
+		<h1>Welcome to Bistro!</h1>
+		<p>Enjoy good food. Instantly!<br><br>Here at bistro, we provide best food at your doorsteps!</p>
+	</div>	
 </div>
-<section><!-- Welcome Section -->
-	<div class="content"><!-- Content Container -->
-	<img src="images/logo/logo.png" width="150">
-	<h1>Welcome to Bistro!</h1>
-	<p>Enjoy good food. Instantly!<br><br>Here at bistro, we provide best food at your doorsteps!</p>
-	</div>
-</section>
 </body>
 </html>
